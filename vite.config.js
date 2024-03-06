c325a96f37f796aa1c2b9ae1d824e8f3607abb1d
Snawlaye <farizfahrian53@gmail.com>
2024-03-06 23:54:08 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
+			__APP_ENV__: process.env.VITE_VERCEL_ENV,
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
